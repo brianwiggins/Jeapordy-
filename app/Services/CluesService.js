f169055236977ca1268ac31f42081415970c905b
brianwiggins <peppermint.butl3r@gmail.com>
2020-02-10 15:49:33 -0700

    b

--- app/Services/CluesService.js
+++ app/Services/CluesService.js
@@ -1,10 +1,16 @@
 import store from "../store.js";
 import Clue from "../Models/Clue.js";
 
+// @ts-ignore
 const _api = axios.create({
-  
+  baseURL:"//jservice.io/api",
+  timeout: 5000
 })
-class CluesService {}
+class CluesService {
+  getApiClues(){
+    console.log("API clues is called")
+  }
+}
 
 const service = new CluesService();
 export default service;
